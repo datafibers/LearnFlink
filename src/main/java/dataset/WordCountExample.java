93180c3a3737917a594489f4f35bd4b25c79b86c
hemantdindi <hemantkumar.dindi@gmail.com>
2016-04-22 03:35:58 +0530

    Word Cound Example - https://ci.apache.org/projects/flink/flink-docs-release-1.0/apis/batch/index.html#example-program

---
+++ src/main/java/dataset/WordCountExample.java
@@ -0,0 +1,35 @@
+package dataset;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.util.Collector;
+
+/**
+ * Created by hemantd on 22/04/2016.
+ */
+public class WordCountExample {
+    public static void main(String[] args) throws Exception {
+        final ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+
+        DataSet<String> text = env.fromElements(
+                "Hello....!",
+                "How are you? Happy Flinking.. :)");
+
+        DataSet<Tuple2<String, Integer>> wordCounts = text
+                .flatMap(new LineSplitter())
+                .groupBy(0)
+                .sum(1);
+
+        wordCounts.print();
+    }
+
+    public static class LineSplitter implements FlatMapFunction<String, Tuple2<String, Integer>> {
+        public void flatMap(String line, Collector<Tuple2<String, Integer>> out) {
+            for (String word : line.split(" ")) {
+                out.collect(new Tuple2<String, Integer>(word, 1));
+            }
+        }
+    }
+}
