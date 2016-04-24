98c3e7a13a5241108bc3674a15f929f7a5f43ba4
hemantdindi <hemantkumar.dindi@gmail.com>
2016-04-24 20:28:47 +0530

    Word Cound Example - https://ci.apache.org/projects/flink/flink-docs-release-1.0/apis/batch/index.html#example-program

---
+++ src/main/java/hemant/dataset/ReadLocalFile.java
@@ -0,0 +1,17 @@
+package hemant.dataset;
+
+import org.apache.flink.api.java.DataSet;
+import org.apache.flink.api.java.ExecutionEnvironment;
+/**
+ * Created by hemantd on 24/04/2016.
+ */
+public class ReadLocalFile {
+
+    public static void main(String []args){
+        ExecutionEnvironment env = ExecutionEnvironment.getExecutionEnvironment();
+        DataSet<String> text = env.readTextFile("F:\\LearnFlink\\01.txt");
+        try {
+            text.print();
+        } catch (Exception e){}
+    }
+}
